2ff3eec06376b4fb8791a40a13b2712d0ab8f2ea
manojvmu85 <manojvmu85@gmail.com>
2019-12-18 12:59:57 +0530

    Convert string to char array by using toCharArray
    Convert string to char array by using toCharArray() method

---
+++ src/com/manoj/string/StringDemo2.java
@@ -0,0 +1,12 @@
+public class Main
+{
+	public static void main(String[] args) {
+		String str = "manoj";
+		char[] c = str.toCharArray();
+		
+		for(char ch : c){
+		    System.out.print(ch+", ");
+		
+		}
+	}
+}
